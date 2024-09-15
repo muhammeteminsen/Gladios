080833a94e1c7b07241da2dba3c9f4ddc39ef8e3
Deniz-GDK <165922967+Deniz-GDK@users.noreply.github.com>
2024-09-15 15:01:04 +0300

    Menu eklendi v1

---
+++ Gladios/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+    
+    public void StartGame()
+    {
+        SceneManager.LoadScene("GameScene");
+        
+    }
+    public void QuitTheGame()
+    {
+        Application.Quit();
+    }
+}
